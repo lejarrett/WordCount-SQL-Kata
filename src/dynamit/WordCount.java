dca2f45e1c733ea7793bb70db8b3f32a90dc7549
Luke Jarrett <lukej_93@yahoo.com>
2019-08-01 15:08:11 -0400

    Setup Complete

---
+++ src/dynamit/WordCount.java
@@ -0,0 +1,10 @@
+package dynamit;
+
+public class WordCount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ test/dynamit/WordCountTests.java
@@ -0,0 +1,14 @@
+package dynamit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WordCountTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
